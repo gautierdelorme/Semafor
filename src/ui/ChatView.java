d6b4af7c4e16fd82aeb1eb3e32ef52ac268584e3
gautierdelorme <gautier.delorme@gmail.com>
2015-11-22 14:47:55 +0100

    update design chatview

--- src/ui/ChatView.java
+++ src/ui/ChatView.java
@@ -10,6 +10,7 @@ package ui;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 /**
  *
@@ -21,26 +22,32 @@ public class ChatView extends JPanel {
     private JList<String> usersList;
     private JTextArea chatBox;
     private JTextField inputBox;
+    private CSButton linkButton;
     
     public ChatView(GUI gui) {
         this.gui = gui;
         
-        String[] data = {"one", "two", "three", "four"};
+        String[] data = {"Arthur", "Bastien", "Pierre", "Henri"};
         usersList = new JList<String>(data);
         usersList.setBackground(new Color(0x3498db));
         usersList.setPreferredSize(new Dimension(100, 0));
         DefaultListCellRenderer renderer =  (DefaultListCellRenderer)usersList.getCellRenderer();
         renderer.setHorizontalAlignment(SwingConstants.CENTER);
 
-        chatBox = new JTextArea(15,20);
+        chatBox = new JTextArea(20,0);
         chatBox.setFont(chatBox.getFont().deriveFont(16.0f));
         chatBox.setEditable(false);
         chatBox.setBorder(javax.swing.BorderFactory.createEmptyBorder());
-        
-        inputBox = new JTextField();
+
+        inputBox = new JTextField(30);
         inputBox.setFont(inputBox.getFont().deriveFont(16.0f));
         inputBox.setBackground(new Color(0xecf0f1));
         inputBox.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        
+        linkButton = new CSButton(FontAwesome.Icon.PAPERCLIP.s());
+        linkButton.setFont(FontAwesome.fontAwesome.deriveFont(20.0f));
+        linkButton.setBackground(inputBox.getBackground());
+        linkButton.setForeground(new Color(0x3498db));
         
         this.setLayout(new GridBagLayout());
         
@@ -55,10 +62,16 @@ public class ChatView extends JPanel {
         gbc.gridy = 0;
         gbc.gridx = 1;
         gbc.gridheight = 1;
+        gbc.gridwidth = 2;
         this.add(new JScrollPane(chatBox), gbc);
         
         gbc.gridy = 1;
         gbc.gridx = 1;
+        gbc.gridwidth = 1;
         this.add(inputBox, gbc);
+        
+        gbc.gridy = 1;
+        gbc.gridx = 2;
+        this.add(linkButton, gbc);
     }
 }
