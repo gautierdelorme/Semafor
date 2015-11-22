17447e89d3b55dc8da61b77c11906bc92cb1b196
Arthur Papailhau <papailhau@gmail.com>
2015-11-22 16:07:05 +0100

    add File Choosers View

--- src/ui/ChatView.java
+++ src/ui/ChatView.java
@@ -11,6 +11,7 @@ package ui;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import javax.swing.*;
 
 /**
@@ -18,7 +19,7 @@ import javax.swing.*;
  * @author gautier
  */
 public class ChatView extends JPanel implements ActionListener {
-    
+
     private FromUser fromUser;
     private JList<String> usersList;
     private JTextArea chatBox;
@@ -26,18 +27,21 @@ public class ChatView extends JPanel implements ActionListener {
     private CSButton linkButton;
     private CSButton disconnectButton;
     private JLabel titleLabel;
-    
+    private final JFileChooser fc;
+
     public ChatView(FromUser fromUser) {
         this.fromUser = fromUser;
-        
+
+        fc = new JFileChooser();
+
         String[] data = {"Arthur", "Bastien", "Pierre", "Henri"};
         usersList = new JList<String>(data);
         usersList.setBackground(new Color(0x3498db));
         usersList.setPreferredSize(new Dimension(100, 0));
-        DefaultListCellRenderer renderer =  (DefaultListCellRenderer)usersList.getCellRenderer();
+        DefaultListCellRenderer renderer = (DefaultListCellRenderer) usersList.getCellRenderer();
         renderer.setHorizontalAlignment(SwingConstants.CENTER);
 
-        chatBox = new JTextArea(20,0);
+        chatBox = new JTextArea(20, 0);
         chatBox.setFont(chatBox.getFont().deriveFont(16.0f));
         chatBox.setEditable(false);
         chatBox.setBorder(javax.swing.BorderFactory.createEmptyBorder());
@@ -51,14 +55,15 @@ public class ChatView extends JPanel implements ActionListener {
         linkButton.setFont(FontAwesome.fontAwesome.deriveFont(20.0f));
         linkButton.setBackground(inputBox.getBackground());
         linkButton.setForeground(new Color(0x3498db));
-        
+        linkButton.addActionListener(this);
+
         disconnectButton = new CSButton(FontAwesome.Icon.POWER_OFF.s());
         disconnectButton.setFont(FontAwesome.fontAwesome.deriveFont(20.0f));
         disconnectButton.setBackground(new Color(0x3498db));
         disconnectButton.setForeground(new Color(0xecf0f1));
         disconnectButton.setHorizontalAlignment(SwingConstants.RIGHT);
         disconnectButton.addActionListener(this);
-        
+
         titleLabel = new JLabel(FontAwesome.Icon.COMMENTS_ALT.s());
         titleLabel.setFont(FontAwesome.fontAwesome.deriveFont(20.0f));
         titleLabel.setBackground(new Color(0x3498db));
@@ -67,45 +72,55 @@ public class ChatView extends JPanel implements ActionListener {
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
         this.setLayout(new GridBagLayout());
-        
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.BOTH;
-        
+
         gbc.gridy = 0;
         gbc.gridx = 0;
         this.add(titleLabel, gbc);
-        
+
         gbc.gridy = 0;
         gbc.gridx = 1;
         gbc.gridwidth = 2;
         this.add(disconnectButton, gbc);
-        
+
         gbc.gridy = 1;
         gbc.gridx = 0;
         gbc.gridwidth = 1;
         gbc.gridheight = 2;
         this.add(usersList, gbc);
-        
+
         gbc.gridy = 1;
         gbc.gridx = 1;
         gbc.gridheight = 1;
         gbc.gridwidth = 2;
         this.add(new JScrollPane(chatBox), gbc);
-        
+
         gbc.gridy = 2;
         gbc.gridx = 1;
         gbc.gridwidth = 1;
         this.add(inputBox, gbc);
-        
+
         gbc.gridy = 2;
         gbc.gridx = 2;
         this.add(linkButton, gbc);
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == disconnectButton) {
             fromUser.disconnect();
+        } else if (e.getSource() == linkButton) {
+            System.out.println("I push openFile button");
+            int returnVal = fc.showOpenDialog(this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //This is where a real application would open the file.
+                System.out.println("Opening: " + file.getName() + ".");
+            } else {
+                System.out.println("Open command cancelled by user.");
+            }
         }
     }
 }
