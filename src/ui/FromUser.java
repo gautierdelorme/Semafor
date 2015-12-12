59f75ff6892eb2251170c82dcf2f0eaf9c62d682
gautierdelorme <gautier.delorme@gmail.com>
2015-12-12 17:08:19 +0100

    add javadoc

--- src/ui/FromUser.java
+++ src/ui/FromUser.java
@@ -17,10 +17,47 @@ import model.User;
  * @author gautier & Arthur
  */
 public interface FromUser {
+    
+    /**
+     * Connect the current user
+     * 
+     * @param nickname nickname of the current user
+     */
     public void connect(String nickname);
+    
+    /**
+     * Disconnect the current user
+     */
     public void disconnect();
+    
+    /**
+     * Send a file
+     * 
+     * @param file the file to send
+     * @param users the users who will receive the file
+     */
     public void sendFile(File file, List<User> users);
+    
+    /**
+     * Send a file request response
+     * 
+     * @param ok true if the user accept the file
+     * @param file the accepted file
+     */
     public void sendFileRequestResponse(boolean ok, File file);
+    
+    /**
+     * Send a message
+     * 
+     * @param message the message to send
+     * @param users the users who will receive the message
+     */
     public void sendMessage(String message, List<User> users);
+    
+    /**
+     * Get the current user
+     * 
+     * @return the current user
+     */
     public User getCurrentUser();
 }
